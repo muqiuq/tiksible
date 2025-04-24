38dcbdf1dcb709d8a462b9ffb2876e34783c9030
alptbz <info@uisa.ch>
2025-04-24 21:36:33 +0200

    partially implemented port config feature

---
+++ Tiksible/Models/CfgEntities/PerPortVlanCfgEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiksible.Models.CfgEntities
+{
+    public class PerPortVlanCfgEntity
+    {
+        public int? PortNumber { get; set; }
+
+        public string? PortName { get; set; }
+
+        public int Untagged { get; set; }
+
+        public List<int> Tagged { get; set; }
+
+    }
+}
---
+++ Tiksible/Models/CfgEntities/PerVlanCfgEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiksible.Models.CfgEntities
+{
+    public class PerVlanCfgEntity
+    {
+
+        public int Vid { get; set; }
+
+        public List<string> Untagged { get; set; }
+
+        public List<string> Tagged { get; set; }
+
+        public string Comment { get; set; }
+
+    }
+}
